09384bcdb382ea744ace84edd5da7334d88513e5
Dmitry <sergeew8@gmail.com>
2021-01-22 13:18:16 +0300

    Task 2

---
+++ Fourth-сhapter-of-the-course/Simplest classes and objects/Task2/src/Test2.java
@@ -0,0 +1,24 @@
+public class Test2 {
+
+    private int a, b;
+    public Test2() {
+        a = 5;
+        b = 5;
+    }
+    public Test2(int a_, int b_) {
+        a = a_;
+        b = b_;
+    }
+    public int get_a() {
+        return a;
+    }
+    public int get_b() {
+        return b;
+    }
+    public void set_a(int a_) {
+        a = a_;
+    }
+    public void set_b(int b_) {
+        b = b_;
+    }
+}
