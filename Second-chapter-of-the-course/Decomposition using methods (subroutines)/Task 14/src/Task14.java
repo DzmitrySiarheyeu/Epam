e6cc2b7c410135f1ea01ab73c2f7b24369cb7992
Dmitry <sergeew8@gmail.com>
2021-01-22 13:11:09 +0300

    Task 14

---
+++ Second-chapter-of-the-course/Decomposition using methods (subroutines)/Task 14/src/Task14.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
+public class Task14 {
+
+    public static void main(String[] args) {
+
+        int k;
+        ArrayList<Integer> array, arrAmstrong;
+
+        array = new ArrayList<Integer>();
+        arrAmstrong = new ArrayList<Integer>();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Search for Amstrong numbers\nInput k:");
+
+        k = input.nextInt();
+
+        while (chek(k)){
+            System.out.println("The number must be greater than 1\nInput k: ");
+        }
+
+        array = fillingArray(k);
+
+        arrAmstrong = findNumAmst(array);
+
+        System.out.println("Numbers Amstrong: " + arrAmstrong);
+
+
+        input.close();
+
+    }
+
+
+
+    private static ArrayList<Integer> findNumAmst(ArrayList<Integer> array) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < array.size(); i++){
+            if(chekNum(array.get(i))){
+                arr.add(array.get(i));
+            }
+        }
+        return arr;
+    }
+
+    private static boolean chekNum(int i) {
+        ArrayList<Integer> arr = splitNum(i);
+        int temp = sumNum(arr);
+
+        if(temp == i) return true;
+        else return false;
+    }
+
+    private static int sumNum(ArrayList<Integer> arr) {
+        int temp = 0;
+        for(int j = 0; j< arr.size(); j++){
+            temp += pow(arr.get(j),arr.size());
+        }
+        return temp;
+    }
+
+    private static ArrayList<Integer> splitNum(int i) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        while (i>0){
+            arr.add(i%10);
+            i = i/10;
+        }
+        return arr;
+    }
+
+    private static ArrayList<Integer> fillingArray(int k) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < k; i++){
+            arr.add(i,i + 1);
+        }
+        return arr;
+    }
+
+    private static boolean chek(int n) {
+        if(n < 1) return true;
+        else return false;
+    }
+}
---
+++ Second-chapter-of-the-course/Tasks. Array of arrays/Task 14/src/Task14.java
@@ -0,0 +1,86 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task14 {
+
+    public static int[] inp;
+    public static Random rnd = new Random();
+    public static int genCount =0;
+
+    public static void main(String[] args) {
+
+        int n, m;
+        int[][] array;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Generating a random matrix with 1 in a column equal to the column number");
+        System.out.println("Input m and n");
+
+        m = input.nextInt();
+        n = input.nextInt();
+
+        while(n > m){
+            System.out.println("The value of n cannot exceed the value of m\nInput m and n");
+            m = input.nextInt();
+            n = input.nextInt();
+        }
+
+        inp = new int[n+1];
+        array = new int[m][n];
+
+        System.out.println("Matrix: ");
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i+1; j++){
+                array[randomInt(n+1)-1][i] = 1;
+            }
+            inp = new int[n+1];
+            genCount=0;
+        }
+
+        for(int i = 0; i < m; i++ ){
+            for(int j = 0; j < n; j++){
+                System.out.printf("%4d", array[i][j]);
+            }
+            System.out.print("\n");
+        }
+
+        input.close();
+
+    }
+
+    public static int randomInt(int in) {
+
+        int count, gen;
+
+        count = in;
+
+        boolean check=true;
+
+        while (genCount < count)
+        {
+            gen=rnd.nextInt(count);
+
+            for (int j : inp) {
+
+                if (j == gen) {
+                    check = true;
+                    break;
+                } else check = false;
+            }
+
+
+            if (!check)
+            {
+                inp[genCount] = gen;
+                genCount++;
+                return gen;
+            }
+        }
+
+        return 0;
+    }
+
+}
