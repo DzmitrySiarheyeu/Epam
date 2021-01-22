fb32f09a6f77d0f59023bb4821fefa356096ba63
Dmitry <sergeew8@gmail.com>
2021-01-22 13:12:32 +0300

    Task 1

---
+++ Fourth-сhapter-of-the-course/Simplest classes and objects/Task1/src/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public int a, b;
+    public int max() {
+        if (a > b)
+            return a;
+        return b;
+    }
+    public int sum() {
+        return a+b;
+    }
+}
