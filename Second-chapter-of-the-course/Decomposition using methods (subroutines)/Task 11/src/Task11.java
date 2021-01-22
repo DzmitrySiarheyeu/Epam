be407b8b64253901907bb2808fed48a9ee61e37e
Dmitry <sergeew8@gmail.com>
2021-01-22 13:10:22 +0300

    Task 11

---
+++ Second-chapter-of-the-course/Decomposition using methods (subroutines)/Task 11/src/Task11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+
+        int a, b;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("A comparison of two numbers");
+        System.out.println("Input a and b: ");
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        if(compNum(a, b) == 0) {
+            System.out.print("The number of digits in the numbers is the same");
+        }else System.out.print("There are more digits in the number " + compNum(a, b));
+
+        input.close();
+    }
+
+    private static int compNum(int a, int b) {
+        if(log10(a)+1 > log10(b)+1) return a;
+        else if (log10(a)+1 == log10(b)+1) return 0;
+        return b;
+    }
+}
---
+++ Second-chapter-of-the-course/Tasks. Array of arrays/Task 11/src/Task11.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+
+        int n = 10, m = 20, last = 0, c = 0;
+        int[][] array;
+        int[] count;
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("");
+
+        array = new int[n][Output of k row and p columnm];
+        count = new int[n];
+
+        System.out.println("Matrix: ");
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++) {
+                array[i][j] = random.nextInt(15);
+                System.out.printf("%4d", array[i][j]);
+                if(last != i){
+                    last++;
+                }else {
+                    if(array[i][j] == 5){
+                        count[i]++;
+                    }
+                }
+            }
+            System.out.print("\n");
+        }
+
+        for(int i = 0; i < count.length; i++){
+            if(count[i] > 3){
+                System.out.printf("In line %d, the number 5 occurs %d times \n", i+1, count[i]);
+                c++;
+            }else if(count[i] == 3){
+                System.out.printf("In line %d, the number 5 occurs 3 times \n", i+1);
+                c++;
+            }
+        }
+
+        if(c == 0){
+            System.out.println("The number 5 does not occur 3 or more times in any line");
+        }
+        input.close();
+
+    }
+}
