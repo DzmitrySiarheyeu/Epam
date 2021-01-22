ec8efee7b3ff5b9d8a32ce85e476cd4561b1eb48
Dmitry <sergeew8@gmail.com>
2021-01-22 21:30:29 +0300

    Task 8

--- Second-chapter-of-the-course/Decomposition using methods (subroutines)/Task 8/src/Task8.java
+++ Second-chapter-of-the-course/Decomposition using methods (subroutines)/Task 8/src/Task8.java
@@ -1,3 +1,13 @@
+/*
+ *   Задание: ЗАдан массив D. Определить следующие суммы: D[1] + D[2] + D[3];
+ *   D[3] + D[4] + D[5]; D[4] + D[5] + D[6].
+ *   Пояснение. Составить метод(методы) для вычисле суммы трех последовательно
+ *   расположенных элементов массива с номерами от k до m.
+ *
+ *   Dzmitry Siarheyeu
+ *   08.11.2020
+ */
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -30,36 +40,35 @@ public class Task8 {
         k = input.nextInt();
         m = input.nextInt();
 
-        while (check(k,m, D)){
-            if(k<1){
+        while (check(k, m, D)){
+            if(k < 1){
                 System.out.println("The initial number cannot be less than 1");
-            }else if(m>D){
+            }else if(m > D){
                 System.out.println("The final number cannot exceed the length of the array");
-            }else if((m-k+1)%3 != 0){
-                System.out.println("The difference between m and k must be a multiple of 3");
+            }else if(m-k+1 < 3){
+                System.out.println("The difference between the values of m and k cannot be less than 3");
             }
+
             System.out.println("Input k and m");
 
             k = input.nextInt();
             m = input.nextInt();
-
         }
 
         sum = sumThreeConsecutiveNumbers(array, k, m);
 
-        for (int i = 0; i < (m-k+1)/3; i++){
-            System.out.printf("Sum of D[%d]+D[%d]+D[%d] = %d\n", k+(i*3), k+(i*3)+1, k+(i*3)+2, sum[i]);
+        for (int i = 0; i <= m-k-2; i++){
+            System.out.printf("Sum of D[%d]+D[%d]+D[%d] = %d\n", k+i, k+i+1, k+i+2, sum[i]);
         }
 
         input.close();
-
     }
 
     private static int[] sumThreeConsecutiveNumbers(int[] array, int k, int m) {
-        int[] sum = new int[(m-k+1)/3];
+        int[] sum = new int[m-k-1];
 
-        for(int i = 0; i < (m-k+1)/3; i++){
-            for (int j = (k-1)+(3*i); j < (k+(3*i))+2; j++){
+        for(int i = 0; i < m-k-1; i++){
+            for (int j = k+i-1; j <= k+i+1; j++){
                 sum[i] = sum[i] + array[j];
             }
         }
@@ -70,7 +79,7 @@ public class Task8 {
     private static boolean check(int k, int m, int D) {
         if(k<1) return true;
         else if(m>D) return true;
-        else if((m-k+1)%3 != 0) return true;
+        else if (m-k+1 < 3) return true;
 
         return false;
     }
